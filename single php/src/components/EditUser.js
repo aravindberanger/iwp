24f47a5a0b82fcfc3b6b681e5da4c710070ec384
billbuttlicker69 <109423569+billbuttlicker69@users.noreply.github.com>
2024-01-02 09:34:48 +0530

    Add files via upload

---
+++ single php/src/components/EditUser.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function ListUser() {
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState([]);
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    function getUser() {
+        axios.get(`http://localhost/api1/user/${id}`).then(function(response) {
+            console.log(response.data);
+            setInputs(response.data);
+        });
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.put(`http://localhost/api1/user/${id}/edit`, inputs).then(function(response){
+            console.log(response.data);
+            navigate('/');
+        });
+        
+    }
+    return (
+        <div>
+            <h1>Edit user</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing="10">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>Address: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.address} type="text" name="address" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>City: </label>
+                            </th>
+                            <td> 
+                                <input value={inputs.city} type="text" name="city" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.mobile} type="text" name="mobile" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2" align ="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    )
+}
